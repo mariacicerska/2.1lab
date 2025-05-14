2cf9f7a5686df169c43761a67c9cad70d8fac5b5
mariacicerska <144207849+mariacicerska@users.noreply.github.com>
2025-05-14 04:31:01 +0300

    Add files via upload

---
+++ 2.1java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 10; // Перше число
+        int num2 = 3;  // Друге число
+
+        int quotient = num1 / num2;  // Результат цілочисельного ділення
+        int remainder = num1 % num2; // Залишок від ділення
+
+        System.out.println("Результат ділення: " + quotient);
+        System.out.println("Залишок від ділення: " + remainder);
+    }
+}
